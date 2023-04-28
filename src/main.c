82b3a9ac592e36e8a4080b8725c26b2ac61e3d3e
treeps <treeps@student.42wolfsburg.de>
2023-04-28 11:41:32 +0200

    deleted builtin_exit from main

--- src/main.c
+++ src/main.c
@@ -31,34 +31,6 @@ static int	init_params(t_pipehelper *params, char **envp)
 	return (1);
 }
 
-static int	builtin_exit(char **parsed_input, int ret)
-{
-	int	i;
-
-	i = 0;
-	if (!parsed_input[1])
-	{
-		ft_putstr_fd("exit\n", 2);
-		ret = 0;
-	}
-	while (parsed_input[1] && parsed_input[1][i])
-	{
-		if (!ft_isdigit(parsed_input[1][i]))
-		{
-			ft_putstr_fd("minishell: exit: ", 2);
-			ft_putstr_fd(parsed_input[1], 2);
-			ft_putstr_fd(": numeric argument required\n", 2);
-			ret = 255;
-			break ;
-		}
-		i++;
-	}
-	if (ret == -1)
-		ret = ft_atoi(parsed_input[1]);
-	free_arr(parsed_input);
-	return (ret);
-}
-
 static int	minishell(t_pipehelper *p, char *input, char **parsed_input)
 {
 	input = get_input(p, NULL, NULL, 0);

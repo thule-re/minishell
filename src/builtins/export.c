8225c9c399805a8465e6b56ab2c8ed4cfa25d603
Anthony Williams <tony@Anthonys-MacBook-Pro-2.local>
2023-05-11 18:57:44 +0200

    export now takes anything as value after key

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: awilliam <awilliam@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 17:50:04 by treeps            #+#    #+#             */
-/*   Updated: 2023/05/08 09:36:58 by awilliam         ###   ########.fr       */
+/*   Updated: 2023/05/11 18:54:37 by awilliam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ static void	*env_add_key(t_env *envp, char **key_val)
 static char	**get_key_val(char *input)
 {
 	char	**key_val;
-	char	**tmp;
+	char	*loc;
 
 	key_val = (char **)ft_calloc(3, sizeof(char *));
 	if (!key_val)
@@ -45,15 +45,16 @@ static char	**get_key_val(char *input)
 		key_val[0] = ft_strdup(input);
 	else
 	{
-		tmp = ft_split(input, '=');
-		if (!tmp)
+		loc = ft_strchr(input, '=');
+		*loc = 0;
+		key_val[0] = ft_strdup(input);
+		if (!key_val[0])
 			return (NULL);
-		key_val[0] = ft_strdup(tmp[0]);
-		if (tmp[1])
-			key_val[1] = ft_strdup(tmp[1]);
+		*loc = '=';
+		if (*(loc + 1))
+			key_val[1] = ft_strdup(loc + 1);
 		else
 			key_val[1] = ft_calloc(1, 1);
-		free_arr(tmp);
 	}
 	if (!key_val[0] && !key_val[1])
 		return (NULL);
